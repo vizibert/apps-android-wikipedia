774d7a6f47d8ad77b983f1034e218ca826e18f97
Viktoriya Zibert <viktoriya.zibert@onetwotrip.com>
2025-02-19 23:13:43 +0300

    hw7: PO

---
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework07/ExploreScreen.kt
@@ -0,0 +1,29 @@
+package org.wikipedia.homeworks.homework07
+
+import com.kaspersky.kaspresso.screens.KScreen
+import io.github.kakaocup.kakao.image.KImageView
+import io.github.kakaocup.kakao.recycler.KRecyclerView
+import org.wikipedia.R
+import org.wikipedia.feed.view.FeedView
+
+object ExploreScreen : KScreen<ExploreScreen>() {
+    override val layoutId = R.layout.fragment_feed
+    override val viewClass = FeedView::class.java
+
+    val toolbarTitle = KImageView {
+        withId(R.id.main_toolbar_wordmark)
+    }
+
+    val items = KRecyclerView(
+        builder = {
+            withId(R.id.feed_view)
+        },
+        itemTypeBuilder = {
+            itemType(::SearchBarItem)
+            itemType(::AnnouncementItem)
+            itemType(::DayHeaderItem)
+            itemType(::TopReadItem)
+            itemType(::NewsItem)
+        }
+    )
+}
